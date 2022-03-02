b00fcc57b0dcf19b4c3a2dde0636afaa9a8e27ee
RabeloGuedes <krepscredian@gmail.com>
2022-03-02 13:51:54 -0300

    Adiciona o requisito bonus 20 e comentarios.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const buttonLogin = document.querySelector('#enter');
 const buttonSend = document.querySelector('#submit-btn');
 const checkBoxAgreement = document.querySelector('#agreement');
+const counter = document.querySelector('#counter');
+const textarea = document.querySelector('#textarea');
 
+// Checks the email and password data.
 function checkData() {
   const email = document.querySelector('#email');
   const password = document.querySelector('#password');
@@ -14,6 +17,7 @@ function checkData() {
 
 buttonLogin.addEventListener('click', checkData);
 
+// Checks if the the agreement button is checked, depending on it's status, enable or diable the send button.
 function enable() {
   if (checkBoxAgreement.checked) {
     buttonSend.disabled = false;
@@ -23,3 +27,12 @@ function enable() {
 }
 
 checkBoxAgreement.addEventListener('click', enable);
+
+// Checks the number os characters and update the counter.
+function typing() {
+  const textareaValue = textarea.value;
+  const characters = textareaValue.split('').length;
+  counter.innerText = 500 - characters;
+}
+
+textarea.addEventListener('keyup', typing);
